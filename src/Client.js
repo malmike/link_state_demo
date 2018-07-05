847b0457ccda5c2d08f3488458d03b6e457a7dc8
Michael Male <michael.male@andela.com>
2018-07-05 11:18:16 +0300

    Add and clear todo items from state

---
+++ src/Client.js
@@ -0,0 +1,95 @@
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloLink } from 'apollo-client-preset';
+import { ApolloClient } from 'apollo-client';
+import { withClientState } from 'apollo-link-state';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+import compose from 'recompose/compose';
+
+// Defaults
+const todoDefaults = {
+  currentTodos: []
+};
+
+// Graphql
+const todoQuery = gql`
+  query GetTodo {
+    currentTodos @client
+  }
+`;
+
+const clearTodoQuery = gql`
+  mutation clearTodo {
+    clearTodo @client
+  }
+`;
+
+const addTodoQuery = gql`
+  mutation addTodo($item: String) {
+    addTodo(item: $item) @client
+  }
+`;
+
+/*
+Resolvers
+*/
+const addTodo = (_obj, { item }, { cache }) => {
+  const query = todoQuery;
+  const { currentTodos } = cache.readQuery({
+    query
+  });
+  const updatedTodos = currentTodos.concat(item);
+  cache.writeQuery({
+    query,
+    data: {
+      currentTodos: updatedTodos
+    }
+  });
+
+  return null;
+};
+
+const clearTodo = (_obj, _args, { cache }) => {
+  cache.writeQuery({
+    query: todoQuery,
+    data: todoDefaults
+  });
+  return null;
+};
+
+const cache = new InMemoryCache();
+
+const stateLink = withClientState({
+  cache,
+  defaults: todoDefaults,
+  resolvers: {
+    Mutation: {
+      addTodo,
+      clearTodo
+    }
+  }
+});
+
+const Client = new ApolloClient({
+  link: ApolloLink.from([stateLink]),
+  cache
+});
+
+const todoQueryHandler = {
+  props: ({ ownProps, data: { currentTodos = [] } }) => ({
+    ...ownProps,
+    currentTodos
+  })
+};
+
+const withTodo = compose(
+  graphql(todoQuery, todoQueryHandler),
+  graphql(addTodoQuery, {
+    name: 'addTodoMutation'
+  }),
+  graphql(clearTodoQuery, {
+    name: 'clearTodoMutation'
+  })
+);
+
+export { Client, withTodo };
